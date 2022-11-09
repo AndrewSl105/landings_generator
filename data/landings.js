9f47bc36dd8a82e9ee4ffc770c36393e7430a1bc
Andrew S <slip1410@gmail.com>
2022-11-09 21:57:30 +0200

    initial commit

---
+++ data/landings.js
@@ -0,0 +1,29 @@
+export const landings = [
+    {
+        id: 1,
+        schema: 'red',
+        title: 'The best product',
+        subTitle: 'subtitle',
+        blocks: [
+            {
+                title: 'example1',
+                type: 'banner',
+                position: '1',
+                id: 1,
+                backdround: '',
+            },
+
+        ]
+    }
+];
+
+export const blocks = [
+    {
+        title: 'example1',
+        type: 'banner',
+        position: '1',
+        id: 1,
+        backdround: '',
+    },
+
+];
